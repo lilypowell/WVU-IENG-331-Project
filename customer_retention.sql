831c6be70f3b6182e7ca96033d31c8b5ae8344ff
oliviadonoghue <ogd00007@mix.wvu.edu>
2026-03-24 16:31:01 -0400

    Add first purchase date CTE to customer retention query

--- customer_retention.sql
+++ customer_retention.sql
@@ -11,8 +11,18 @@ WITH customer_orders AS (
 	JOIN customers as c
 		ON o.customer_id = c.customer_id
 	WHERE o.order_purchase_timestamp IS NOT NULL
+),
+
+--Add first purchase date CTE
+first_orders AS (
+	SELECT 
+		customer_unique_id,
+		MIN(order_date) AS first_order_date
+	FROM customer_orders
+	GROUP BY customer_unique_id
 )
+
 SELECT *
-FROM customer_orders
+FROM first_orders
 LIMIT 10;
 
