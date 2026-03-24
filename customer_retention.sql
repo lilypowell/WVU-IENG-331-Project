997bd567bbad51a36c87bd0c3a47469e399e8055
oliviadonoghue <ogd00007@mix.wvu.edu>
2026-03-24 16:27:00 -0400

    Start customer retention query with customer order history CTE

--- customer_retention.sql
+++ customer_retention.sql
@@ -1,22 +1,18 @@
---Business Question #3: How likely are  customers to repeat purchases within different time frames of their first purchase?
---This query will identify each customers first purchase date and find whether the customer placed a later order.
---It will calculate the number and percentage of customers who returned within the following time frames
---1. 30 days
---2. 60 days
---3. 90 days
+--Buisness Question #3: How likely are customers to place a second order within a given timeframe after their first purchase?
+--This query will identify first purchase dates and check whether a customer returns within 30, 60, or 90 days.
 
---Identify customer orders
+--Identify customer order
 WITH customer_orders AS (
-	SELECT
+	SELECT 
 		c.customer_unique_id,
 		o.order_id,
 		DATE(o.order_purchase_timestamp) AS order_date
-	FROM orders AS o
-	JOIN customers AS c
+	FROM orders as o
+	JOIN customers as c
 		ON o.customer_id = c.customer_id
 	WHERE o.order_purchase_timestamp IS NOT NULL
 )
-SELECT * 
+SELECT *
 FROM customer_orders
 LIMIT 10;
 
